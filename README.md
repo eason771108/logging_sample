595a9c048702ec010e7191cb65d03f7db153af32
Ethan <Ethan.Wu@ttpsc.com>
2020-03-17 14:51:46 +0800

    init comit

---
+++ README.md
@@ -0,0 +1,3 @@
+###This is a sample project for how to use sl4j logging in Java project.
+
+You can check the information [here](https://logging.apache.org/log4j/2.x/manual/configuration.html).
---
+++ src/main/java/runApplication/mainApplication.java
@@ -0,0 +1,15 @@
+package runApplication;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class mainApplication {
+
+    static private Logger log = LoggerFactory.getLogger(mainApplication.class);
+
+    public static void main(String[] args) {
+
+        log.info("Hello world");
+    }
+}
